c1fbe08675d7ce275ad5e08ef67e5d416bc3bd68
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-03-04 17:26:30 -0300

    first commit

---
+++ android/app/src/main/java/com/barber_shop_mobile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.barber_shop_mobile;
+
+import com.facebook.react.ReactActivity;
+
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Barber_shop_mobile";
+  }
+
+
+}
