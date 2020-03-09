1911bdd0f218421e49bc6ba5617b2439687febe3
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-03-09 16:31:34 -0300

    schedules style

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {View} from 'react-native';
 
-// import { Container } from './styles';
+import {Container, Title} from './styles';
 import Background from '~/components/Background';
 
 export default function Profile() {
-    return <Background />;
+    return (
+        <Background>
+            <Container>
+                <Title>Perfil</Title>
+            </Container>
+        </Background>
+    );
 }
