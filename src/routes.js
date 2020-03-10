570d311163137dac94692785c2cd10fed5ef58cb
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-03-10 16:56:44 -0300

    new routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -6,6 +7,11 @@ import {useSelector} from 'react-redux';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 
@@ -16,6 +22,35 @@ const Tab = createBottomTabNavigator();
 
 export default function Routes() {
     const signed = useSelector(state => state.auth.signed);
+
+    function New({navigation}) {
+        return (
+            <Stack.Navigator
+                screenOptions={{
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                        marginLeft: 20,
+                    },
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Agendamentos')}>
+                            <Icon name="chevron-left" size={20} color="#fff" />
+                        </TouchableOpacity>
+                    ),
+                }}>
+                <Stack.Screen
+                    name="Selecione o Prestador"
+                    component={SelectProvider}
+                />
+                <Stack.Screen
+                    name="SelectDateTime"
+                    component={SelectDateTime}
+                />
+                <Stack.Screen name="Confirm" component={Confirm} />
+            </Stack.Navigator>
+        );
+    }
 
     return (
         <NavigationContainer>
@@ -40,6 +75,20 @@ export default function Routes() {
                         options={{
                             tabBarIcon: ({color}) => (
                                 <Icon name="event" size={20} color={color} />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Agendar"
+                        component={New}
+                        options={{
+                            tabBarVisible: false,
+                            tabBarIcon: ({color}) => (
+                                <Icon
+                                    name="add-circle-outline"
+                                    size={20}
+                                    color={color}
+                                />
                             ),
                         }}
                     />
